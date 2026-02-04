b19d2e479dc16b7696fee824e399a068dca45f18
Jiahao9 <simjiahao9@gmail.com>
2026-02-04 18:24:55 +0900

    [UPD] prompt_text default value

--- launch/sam3.launch.py
+++ launch/sam3.launch.py
@@ -84,6 +84,7 @@ def generate_launch_description():
         ),
         DeclareLaunchArgument(
             "prompt_text",
+            default_value="['object']",
             # default_value="['object on the table']",
             # default_value="['metal cup', 'banana', 'pen', 'paper cup', 'headphone', 'dice', 'pringles potato chips', 'game controller']",
             # default_value="['red object']",
