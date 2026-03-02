2d69bed3a2a79dcc960fcee2ddf52ffe0d54bf2b
Dai632 <s.daiyo.h@gmail.com>
2026-03-03 02:05:20 +0900

    [UPD]Update to add  package installed in install.sh.

--- install.sh
+++ install.sh
@@ -9,6 +9,8 @@ cd image_to_position/ && bash install.sh && cd ..
 
 pip3 install torch --break-system-packages
 pip3 install ultralytics "numpy<2" --break-system-packages
+pip install timm --break-system-packages
+pip install "git+https://github.com/ultralytics/CLIP.git" --break-system-packages 
 
 sudo apt install ros-$ROS_DISTRO-vision-msgs
 
